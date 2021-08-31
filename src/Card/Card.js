4df3e64f86ca3af8f1cc89badeebcf33be1a25ca
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-08-31 09:24:31 +0100

    Created react app, installed axios and created card component

---
+++ src/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card() {
+  return (
+    <div>
+      <div className="Card">
+        <h2>Name</h2>
+        <p>Email</p>
+      </div>
+    </div>
+  );
+}
